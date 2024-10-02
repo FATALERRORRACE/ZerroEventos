b496eb6df97631bb899e4e4188a5fc506b628067
FATALERRORRACE <frdrummer@gmail.com>
2024-10-01 23:24:51 -0500

    trying to get x,y from image

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
 import './bootstrap';
+import Cropper from 'cropperjs';
+import 'cropperjs/dist/cropper.min.css';
+
+
+jQuery(document).ready(()=>{
+    // initialize plugin with defaults
+    // jQuery("#file").fileinput();
+    // // with plugin options
+    // jQuery("#file").fileinput({'showUpload':false, 'previewFileType':'any'})
+    // .on('fileloaded', function(event, file, previewId, fileId, index, reader) {
+    //     console.log("fileloaded");
+    // });
+    const image = document.getElementById('image');
+    const cropper = new Cropper(
+        image, 
+        {
+
+        },
+    );
+}); 
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,7 +2,16 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('app.head')
     <body class="antialiased">
-        
+        <div class="container text-center" >
+            <div class="row">
+              <div class="col">
+                <input type="file" id="file">
+              </div>
+              <div>
+                <img hidden id="image" src="img/Formato-para-describir-las-actividades-de-un-proceso-35-MANUAL-DE-PROCESOS-El-manual-de.png">
+              </div>
+            </div>
+          </div>
     </body>
     @include('app.js')
 </html>
