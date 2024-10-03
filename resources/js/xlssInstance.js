9e4c425d28199c3ad63dce6f2e8b6e0c23286cd4
FATALERRORRACE <frdrummer@gmail.com>
2024-10-03 11:12:31 -0500

    idk if add al instances of libraries in one clas

--- resources/js/xlssInstance.js
+++ resources/js/xlssInstance.js
@@ -1,58 +1,64 @@
 export default class XlssClassCreator{
-    CreateTable(){
-        var data = [
-            ['Jazz', 'Honda', '2019-02-12', '', true, '$ 2.000,00', '#777700'],
-            ['Civic', 'Honda', '2018-07-11', '', true, '$ 4.000,01', '#007777'],
-        ];
-        var xlssElement = jspreadsheet(document.getElementById('spreadsheet'), {
-            data:data,
-            columns: [
-                {
-                    type: 'text',
-                    title:'Car',
-                    width:90
-                },
-                {
-                    type: 'dropdown',
-                    title:'Make',
-                    width:120,
-                    source:[
-                        "Alfa Romeo",
-                        "Audi",
-                        "Bmw",
-                        "Chevrolet",
-                        "Chrystler",
-                        // (...)
-                    ]
-                },
-                {
-                    type: 'calendar',
-                    title:'Available',
-                    width:120
-                },
-                {
-                    type: 'image',
-                    title:'Photo',
-                    width:120
-                },
-                {
-                    type: 'checkbox',
-                    title:'Stock',
-                    width:80
-                },
-                {
-                    type: 'numeric',
-                    title:'Price',
-                    mask:'$ #.##,00',
-                    width:80,
-                    decimal:','
-                },
-                {
-                    type: 'color',
-                    width:80,
-                    render:'square',
-                },
+
+    data = [
+        ['Jazz', 'Honda', '2019-02-12', '', true, '$ 2.000,00', '#777700'],
+        ['Civic', 'Honda', '2018-07-11', '', true, '$ 4.000,01', '#007777'],
+    ];
+
+    column = [
+        {
+            type: 'text',
+            title:'Car',
+            width:90
+        },
+        {
+            type: 'dropdown',
+            title:'Make',
+            width:120,
+            source:[
+                "Alfa Romeo",
+                "Audi",
+                "Bmw",
+                "Chevrolet",
+                "Chrystler",
+                // (...)
             ]
+        },
+        {
+            type: 'calendar',
+            title:'Available',
+            width:120
+        },
+        {
+            type: 'image',
+            title:'Photo',
+            width:120
+        },
+        {
+            type: 'checkbox',
+            title:'Stock',
+            width:80
+        },
+        {
+            type: 'numeric',
+            title:'Price',
+            mask:'$ #.##,00',
+            width:80,
+            decimal:','
+        },
+        {
+            type: 'color',
+            width:80,
+            render:'square',
+        },
+    ];
+
+    xlssElement;
+
+    CreateTable(){
+        this.xlssElement = jspreadsheet(document.getElementById('spreadsheet'), {
+            data: this.data,
+            columns: this.column
         });
     }
 }
