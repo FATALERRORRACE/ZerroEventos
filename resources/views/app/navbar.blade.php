00951d1471c31eec6b38d838c91dfa0111b2185b
FATALERRORRACE <frdrummer@gmail.com>
2024-10-07 13:15:28 -0500

    one more icon

--- resources/views/app/navbar.blade.php
+++ resources/views/app/navbar.blade.php
@@ -11,26 +11,31 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Nuevo Documento</a>
+          <a class="nav-link active" aria-current="page" href="/home">Nuevo Documento</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" target="_blank" href="https://www.linkedin.com/in/pablo-trivi%C3%B1o-bravo/">LinkedIn</a>
+          <a class="nav-link active" aria-current="page" href="/">¿Qué es?</a>
         </li>
+        <!--<li class="nav-item">
+          <a class="nav-link" target="_blank" href="https://www.linkedin.com/in/pablo-trivi%C3%B1o-bravo/">LinkedIn</a>
+        </li>-->
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Contacto
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">f
             <li><a class="dropdown-item" target="_blank" href="https://www.linkedin.com/in/pablo-trivi%C3%B1o-bravo/"><img src="img\icons8-linkedin-50.png"> LinkedIn</a></li>
             <li><a class="dropdown-item" target="_blank" href="https://wa.me/+573003684254"><img src="img\icons8-whatsapp-50.png"> Whatsapp</a></li>
+            <li><a class="dropdown-item" target="_blank" href="https://github.com/FATALERRORRACE"><img src="img\icons8-github-50.png"> GitHub</a></li>
             <li><a class="dropdown-item" target="_blank" ><img src="img\icons8-correo-50.png"> Copiar E-mail</a></li>
+            
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <!--<li><a class="dropdown-item" href="#">Something else here</a></li>-->
           </ul>
         </li>
-        <li class="nav-item">
+        <!--<li class="nav-item">
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
+        </li>-->
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
